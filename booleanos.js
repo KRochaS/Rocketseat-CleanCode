1cb90ae55ae1568a4ee5949b22ad6c3f643cfdf2
KRochaS <karinerocha.s@hotmail.com>
2022-12-06 20:47:57 -0300

    boolean/nomenclatura

---
+++ booleanos.js
@@ -0,0 +1,27 @@
+// Boleanos
+
+// Sempre escrever como se fosse uma pergunta 
+// com resposta Sim ou Não
+
+const userMajority = true;
+
+const isUserOnMajority = true; 
+
+
+// flags: is, does, has
+
+
+const disabled = true;
+
+const isDisabled = true;
+
+const userHasGroup = true;
+const doesUserHasGroup = true;
+const isLoading = true;
+
+// Booleans sempre como perguntas / semântico
+
+// evitar variáveis genéricas: loading, disabled, data, list
+
+const isUserProfileDisabled = true;
+const isUserProfileLoading = true;
