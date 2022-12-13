26c761741fe9558471feae02c31be94e132b6be8
KRochaS <karinerocha.s@hotmail.com>
2022-12-13 09:53:35 -0300

    magic numbers - clean code

---
+++ numeros-magicos.js
@@ -0,0 +1,32 @@
+// simples 
+
+// setTimeOut, setInterval
+
+
+setTimeout(() => {
+
+}, 2592000000)
+
+setTimeout(() => {
+
+}, 1000 * 60 * 24 * 30)
+
+
+const INTERVAL_30_DAYS = 1000 * 60 * 24 * 30;
+
+setTimeout(() => {
+
+}, INTERVAL_30_DAYS)
+
+
+
+// avançado
+
+
+function calculateDiscount(price, discountAmount) {
+    // retorna desconto
+}
+
+function calculateDiscount(priceInCents, discountAmountInPercentage) {
+    // retorna desconto
+}
