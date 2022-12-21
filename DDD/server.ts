6bc05f5835749874b1edda7513fda52d44f4b390
KRochaS <karinerocha.s@hotmail.com>
2022-12-20 22:20:28 -0300

    DDD

---
+++ DDD/server.ts
@@ -0,0 +1,7 @@
+import { SubmitOrder } from "./use-cases/submit-order";
+import { PostOrdersRepository } from "./repositories/postgres/postgres-order-repository";
+const submitOrder = new SubmitOrder(
+    new PostOrdersRepository()
+)
+
+// SubmitOrder.execute()
